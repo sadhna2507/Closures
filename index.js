3482f6b53c0437c403ba614400f6339445832afb
Sadhna kumari <sadhhnakhairia1998@gmail.com>
2022-08-09 23:52:45 +0530

    closure

--- index.js
+++ index.js
@@ -32,39 +32,49 @@
 // everytime it will give 2 as output
 
 
-var count = 1;
+// var count = 1;
 
-function counter(){
-    count++;
-    console.log(count);
+// function counter(){
+//     count++;
+//     console.log(count);
 
-}
-// console.log(counter);
-counter(); // output =2
-counter(); // output =3
-counter(); // output =4
-counter(); // output =5
-console.log(count);  // output =5 
-
-
-
-
-var count = 1;
-
-function counter(){
-    var age = 20;
-    function check(){
-        console.log("inside check", age)
-    }
-    check();
-    count++;
-    console.log(count);
-
-}
+// }
 // console.log(counter);
 // counter(); // output =2
 // counter(); // output =3
 // counter(); // output =4
 // counter(); // output =5
-// console.log(age);
 // console.log(count);  // output =5 
+
+
+
+
+// var count = 1;
+
+// function counter(){
+//     var age = 20;
+//     function check(){
+//         console.log("inside check", age, count)// here i can also access count because it is present on the top..means in global scope
+//     }
+//     check();
+//     count++;
+//     console.log(count);
+
+// }
+// counter();
+// counter();
+// counter();
+// // console.log(age);
+// // console.log(count);  // output =5
+
+
+function createCounter(){
+    let count = 1;
+
+    function counterIncrement(){
+        count++;
+        return count;
+    }
+    return counterIncrement;
+}
+console.log(createCounter)
